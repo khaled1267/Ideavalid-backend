03b18c7abeb9594841fbbb3ad4a715fa8c7a6dce
khaled1267 <khaledkhan1267@gmail.com>
2026-05-22 14:08:52 +0600

    jwt added

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express()
 const cors = require('cors')
 const dotenv = require('dotenv')
 dotenv.config()
+
+
 const port = process.env.PORT || 5000
 
 // Middleware
@@ -10,6 +12,7 @@ app.use(express.json())
 app.use(cors())
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { jwtVerify, createRemoteJWKSet } = require('jose-cjs')
 const uri = process.env.MONGODB_URI
 
 const client = new MongoClient(uri, {
@@ -19,6 +22,29 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   }
 });
+
+const JWKS = createRemoteJWKSet(new URL(`${process.env.CLIENT_URL}/api/auth/jwks`));
+
+const verifyToken = async (req, res, next) => {
+  const authHeader = req?.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+  const token = authHeader.split(" ")[1];
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  try {
+    const { payload } = await jwtVerify(token, JWKS);
+    console.log(payload);
+    next();
+  } catch (error) {
+    return res.status(403).json({ message: "Forbidden" });
+  }
+};
+
+
 
 async function run() {
   try {
@@ -55,7 +81,7 @@ async function run() {
       try {
         
 
-        const result = await ideasCollection.insertOne(ideaData);
+        const result = await ideasCollection.insertOne();
 
         
         res.status(201).json({ success: true, ...result });
@@ -75,7 +101,8 @@ async function run() {
       res.json(result);
     });
 
-   app.get('/my-ideavalid/:userid', async (req, res) => {
+   app.get('/my-ideavalid/:userid',verifyToken, async (req, res) => {
+    
   try {
     const { userid } = req.params; 
 
